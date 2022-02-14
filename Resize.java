f631713514f2826fa54c13abb7e4997b3ebcb5b7
Saravanan Suresh <98247476+SaravananSuresh7@users.noreply.github.com>
2022-02-14 23:38:00 +0530

    Add files via upload

---
+++ Resize.java
@@ -0,0 +1,27 @@
+package week4.day2.assignments;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Resize {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://jqueryui.com/resizable");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.switchTo().frame(0);
+		WebElement element = driver
+				.findElement(By.xpath("//div[contains(@class,'ui-resizable-handle ui-resizable-se')]"));
+		Actions builder = new Actions(driver);
+		builder.clickAndHold(element).moveByOffset(50, 50).release().perform();
+
+	}
+
+}
